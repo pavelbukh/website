b0e5a63a555183570c7322fa49032fe7836f08f4
pavelbukhteev <p.bukhteev@elomobility.com>
2024-03-04 16:52:07 +0100

    Starting

---
+++ website-canvas/src/redux/reducers/counterReducer.jsx
@@ -0,0 +1,13 @@
+const initialState = {value: 0}
+
+function counterReducer(state = initialState, action){
+  if (action.type === 'counter/increment'){
+    return {
+      ...state,
+      value: state.value + 1
+    }
+  }
+  return state
+}
+
+export default counterReducer
